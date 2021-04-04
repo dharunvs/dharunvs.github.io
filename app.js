e54f55a637a658bd552bd9fa980ebbbb5350d217
Dharun Sivakumar <dharunvs@users.noreply.github.com>
2021-04-04 12:46:53 +0530

    Updates

---
+++ app.js
@@ -0,0 +1,6 @@
+navAnimate = () => {
+  const navLinks = document.getElementById("nav-links");
+  const menuButton = document.getElementById("menu-button");
+
+  navLinks.classList.toggle("nav-move");
+};
