2d210b90125db1ee43c8ac10f9705d70561b9bac
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-04-08 14:12:11 +0530

    Updates

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-navAnimate = () => {
-  const navLinks = document.getElementById("nav-links");
-  const content = document.getElementById("content");
-  const burger = document.getElementById("burger");
+const navLinks = document.getElementById("nav-links");
+const content = document.getElementById("content");
+const burger = document.getElementById("burger");
 
+navAnimate = () => {
   navLinks.classList.toggle("nav-move");
   content.classList.toggle("content-filter");
   burger.classList.toggle("b-anim");
 };
+
+pageAnimate = (page) => {
+  const pages = document.getElementById('pages')
+  if (page == 'home'){
+    pages.style.transform = 'translateY(0)'
+  } else if (page == 'work'){
+    pages.style.transform = 'translateY(-100vh)'
+  } else if (page == 'about'){
+    pages.style.transform = 'translateY(-200vh)'
+  }
+
+  navLinks.classList.toggle("nav-move");
+  content.classList.toggle("content-filter");
+  burger.classList.toggle("b-anim");
+  
+}
