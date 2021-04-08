767b803f9acab978dd0270138df805b887544e4d
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-04-08 22:05:15 +0530

    Updates

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 const navLinks = document.getElementById("nav-links");
 const content = document.getElementById("content");
 const burger = document.getElementById("burger");
-const pages = document.getElementById('pages')
+const pages = document.getElementById("pages");
 
-pages.style.transform = 'translateY(-100vh)'
+pages.style.transform = "translateY(-100vh)";
 
-navAnimate = () => {
+function navAnimate() {
   navLinks.classList.toggle("nav-move");
   content.classList.toggle("content-filter");
   burger.classList.toggle("b-anim");
-};
+}
 
-pageAnimate = (page) => {
-  if (page == 'home'){
-    pages.style.transform = 'translateY(0)'
-  } else if (page == 'work'){
-    pages.style.transform = 'translateY(-100vh)'
-  } else if (page == 'about'){
-    pages.style.transform = 'translateY(-200vh)'
+function pageAnimate(page) {
+  if (page == "home") {
+    pages.style.transform = "translateY(0)";
+  } else if (page == "work") {
+    pages.style.transform = "translateY(-100vh)";
+  } else if (page == "contact") {
+    pages.style.transform = "translateY(-200vh)";
   }
 
   navLinks.classList.toggle("nav-move");
   content.classList.toggle("content-filter");
   burger.classList.toggle("b-anim");
-  
 }
