8cdaedf319df7e668d6fcac5f673a13891ca3be7
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-04-08 17:33:18 +0530

    Updates

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const navLinks = document.getElementById("nav-links");
 const content = document.getElementById("content");
 const burger = document.getElementById("burger");
+const pages = document.getElementById('pages')
+
+pages.style.transform = 'translateY(-100vh)'
 
 navAnimate = () => {
   navLinks.classList.toggle("nav-move");
@@ -9,7 +12,6 @@ navAnimate = () => {
 };
 
 pageAnimate = (page) => {
-  const pages = document.getElementById('pages')
   if (page == 'home'){
     pages.style.transform = 'translateY(0)'
   } else if (page == 'work'){
